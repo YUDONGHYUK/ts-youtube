c61bd43bf56346e8462abaaade62e1f12b203b6d
doyu <ydh0905@nate.com>
2022-12-05 02:40:34 +0900

    Setup router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import App from './App';
+import NotFound from './pages/NotFound';
+import Videos from './pages/Videos';
+import VideoDetail from './pages/VideoDetail';
+import './index.css';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />} errorElement={<NotFound />}>
+      <Route index element={<Videos />} />
+      <Route path='videos' element={<Videos />} />
+      <Route path='videos/:keyword' element={<Videos />} />
+      <Route path='videos/watch/:videoId' element={<VideoDetail />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
