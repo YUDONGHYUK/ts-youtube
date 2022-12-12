f8d5006b3c10711190f714e9ab6c821493899dc9
doyu <ydh0905@nate.com>
2022-12-12 16:56:54 +0900

    Test VideoItem component

---
+++ src/components/VideoItem/VideoItem.test.tsx
@@ -0,0 +1,105 @@
+import { render, screen } from '@testing-library/react';
+import {
+  createMemoryRouter,
+  RouterProvider,
+  useLocation,
+} from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { timeAgo } from '../../utils/timeAgo';
+import { fakeVideo as video } from '../../tests/videoData';
+import VideoItem from './VideoItem';
+
+const { publishedAt, title, thumbnails, channelTitle } = video.snippet;
+
+describe('VideoItem', () => {
+  it('renders correctly', () => {
+    const routes = [
+      {
+        path: '/',
+        element: <VideoItem video={video} />,
+      },
+    ];
+
+    const router = createMemoryRouter(routes, { initialEntries: ['/'] });
+
+    render(<RouterProvider router={router} />);
+
+    const imgElement = screen.getByRole('img');
+    expect(imgElement).toHaveAttribute('src', thumbnails.medium.url);
+    expect(imgElement).toHaveAttribute('alt', title);
+
+    const titleElement = screen.getByText(title);
+    expect(titleElement).toBeInTheDocument();
+
+    const channelTitleElement = screen.getByText(channelTitle);
+    expect(channelTitleElement).toBeInTheDocument();
+
+    const publishElement = screen.getByText(timeAgo(publishedAt));
+    expect(publishElement).toBeInTheDocument();
+  });
+
+  it('renders list type correctly', () => {
+    const routes = [
+      {
+        path: '/',
+        element: <VideoItem video={video} type='list' />,
+      },
+    ];
+
+    const router = createMemoryRouter(routes, { initialEntries: ['/'] });
+
+    render(<RouterProvider router={router} />);
+
+    const videoItemElement = screen.getByRole('listitem');
+    expect(videoItemElement).toHaveClass('flex gap-2 mb-2', { exact: false });
+
+    const imgElement = screen.getByRole('img');
+    expect(imgElement).toHaveAttribute('src', thumbnails.medium.url);
+    expect(imgElement).toHaveAttribute('alt', title);
+    expect(imgElement).toHaveClass('w-40', { exact: false });
+
+    const titleElement = screen.getByText(title);
+    expect(titleElement).toBeInTheDocument();
+    expect(titleElement).toHaveClass('text-sm', { exact: false });
+
+    const channelTitleElement = screen.getByText(channelTitle);
+    expect(channelTitleElement).toBeInTheDocument();
+    expect(channelTitleElement).toHaveClass('text-xs', { exact: false });
+
+    const publishElement = screen.getByText(timeAgo(publishedAt));
+    expect(publishElement).toBeInTheDocument();
+    expect(publishElement).toHaveClass('text-xs', { exact: false });
+  });
+
+  it('navigate to detail page with video state when clicked', async () => {
+    userEvent.setup();
+
+    // useLocation으로 전달된 state를 보여주는 테스트용 Component
+    function LocationStateDisplay() {
+      return <pre>{JSON.stringify(useLocation().state)}</pre>;
+    }
+
+    const routes = [
+      {
+        path: '/',
+        element: <VideoItem video={video} />,
+      },
+      {
+        path: `/videos/watch/${video.id}`,
+        element: <LocationStateDisplay />,
+      },
+    ];
+
+    const router = createMemoryRouter(routes, { initialEntries: ['/'] });
+
+    render(<RouterProvider router={router} />);
+
+    const videoItemElement = screen.getByRole('listitem');
+    await userEvent.click(videoItemElement);
+
+    const videoStateFromLocation = screen.getByText(JSON.stringify(video));
+    expect(videoStateFromLocation).toBeInTheDocument();
+  });
+});
+
+export {};
---
+++ src/tests/videoData.js
@@ -0,0 +1,54 @@
+export const fakeVideo = {
+  id: '1',
+  snippet: {
+    publishedAt: '2007-08-23T00:34:43Z',
+    channelId: 'channelId',
+    title: 'title',
+    description: 'description',
+    thumbnails: {
+      medium: {
+        url: 'http://image/',
+        width: 'width',
+        height: 'height',
+      },
+    },
+    channelTitle: 'channelTitle',
+  },
+};
+
+export const fakeVideos = [
+  {
+    id: '1',
+    snippet: {
+      publishedAt: '2007-08-23T00:34:43Z',
+      channelId: 'channelId',
+      title: 'title',
+      description: 'description',
+      thumbnails: {
+        medium: {
+          url: 'http://image/',
+          width: 'width',
+          height: 'height',
+        },
+      },
+      channelTitle: 'channelTitle',
+    },
+  },
+  {
+    id: '2',
+    snippet: {
+      publishedAt: '2010-08-23T00:34:43Z',
+      channelId: 'channelId2',
+      title: 'title2',
+      description: 'description2',
+      thumbnails: {
+        medium: {
+          url: 'http://image2/',
+          width: 'width',
+          height: 'height',
+        },
+      },
+      channelTitle: 'channelTitle',
+    },
+  },
+];
